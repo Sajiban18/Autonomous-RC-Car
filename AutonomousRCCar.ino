513f9c8bac1199b8c0d3a944bed9bb356c3c6f70
Sajiban18 <p.sajiban18@gmail.com>
2020-05-01 15:55:54 +0100

    Corrections

--- AutonomousRCCar.ino
+++ AutonomousRCCar.ino
@@ -21,8 +21,8 @@ int USEcho = 8;
 
 long duration;
 int distance;
-int distances[5];
-int objectDistance = 15; 
+int distances[5] = {150, 150, 150, 150, 150};
+int objectDistance = 7; 
 
 AccelStepper stepper1(HALFSTEP, mtrPin1, mtrPin3, mtrPin2, mtrPin4);
 
@@ -90,16 +90,29 @@ void directionChoice() {
   if((distances[0] + distances[1]) > (distances[3] + distances[4])) {
     leftForward();
     Serial.println("LeftForward");
-    //delay(2000);
+    delay(90);
   } else if((distances[0] + distances[1]) < (distances[3] + distances[4])){
     rightForward();
     Serial.println("RightForward");
-    //delay(2000);
+    delay(90);
   } else {
     int choice = rand() % 10 + 1;
     choice % 2 == 0 ? leftForward() : rightForward();
     Serial.println("Random");
   }
+}
+
+void checkCurrentPosition() {
+  if(stepper1.currentPosition() == -340)
+    distances[0] = calculateDistance();
+  if(stepper1.currentPosition() == -225)
+    distances[1] = calculateDistance();
+  if(stepper1.currentPosition() == 0)
+    distances[2] = calculateDistance();
+  if(stepper1.currentPosition() == 225)
+    distances[3] = calculateDistance();
+  if(stepper1.currentPosition() == 340)
+    distances[4] = calculateDistance();
 }
 
 
@@ -128,16 +141,7 @@ void loop() {
   stepper1.run();  //Start
 
   //Store distances of data in distances array 
-  if(stepper1.currentPosition() == -340)
-    distances[0] = calculateDistance();
-  if(stepper1.currentPosition() == -225)
-    distances[1] = calculateDistance();
-  if(stepper1.currentPosition() == 0)
-    distances[2] = calculateDistance();
-  if(stepper1.currentPosition() == 225)
-    distances[3] = calculateDistance();
-  if(stepper1.currentPosition() == 340)
-    distances[4] = calculateDistance();
+  checkCurrentPosition();
 
   if(distances[0] < objectDistance || distances[1] < objectDistance || distances[2] < objectDistance || distances[3] < objectDistance || distances[4] < objectDistance) {
     
@@ -159,22 +163,38 @@ void loop() {
     if((distances[0] >= objectDistance && distances[1] >= objectDistance && distances[2] >= objectDistance) && (distances[3] < objectDistance || distances[4] < objectDistance)) {
       if(distances[4] < objectDistance) {
         leftForward();
-        //delay(1000);
+        Serial.println("leftForward1");
+        delay(45);
       } else {
         leftForward();
-        //delay(1500);
+        Serial.println("leftForward2");
+        delay(90);
       }
+    } 
+    
+    if((distances[0] >= objectDistance && distances[1] >= objectDistance && distances[2] < objectDistance) && (distances[3] < objectDistance || distances[4] < objectDistance)) {
+      rightBackward();
+      Serial.println("rightBackward");
+      delay(90);
     }
 
     //Object onLeft
     if((distances[4] >= objectDistance && distances[3] >= objectDistance && distances[2] >= objectDistance) && (distances[0] < objectDistance || distances[1] < objectDistance)) {
       if(distances[0] < objectDistance) {
         rightForward();
-        //delay(1000);
+        Serial.println("rightForward1");
+        delay(45);
       } else {
         rightForward();
-        //delay(1500);
+        Serial.println("rightForward2");
+        delay(90);
       }
+    } 
+    
+    if((distances[4] >= objectDistance && distances[3] >= objectDistance && distances[2] < objectDistance) && (distances[0] < objectDistance || distances[1] < objectDistance)) {
+      leftBackward();
+      Serial.println("leftBackward");
+      delay(90);
     }
 
     // Fully blocked at the front left and right
@@ -182,15 +202,16 @@ void loop() {
     || (distances[0] >= objectDistance && distances[1] < objectDistance && distances[2] < objectDistance && distances[3] < objectDistance && distances[4] >= objectDistance)) {
       //stepper1.moveTo(0);
       applyBreak();
-      //delay(500);
-      while(calculateDistance() < objectDistance) {
+      delay(500);
+      while(distances[1] < objectDistance && distances[2] < objectDistance && distances[3] < objectDistance) {
         backward();
+        Serial.println("Backward");
       }
       applyBreak();
-      //delay(500);
       directionChoice();
     }  
   } else {
     forward();
+    Serial.println("Forward");
   }
 }
